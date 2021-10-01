d45e696492700e6af9d3d437230e67ae0dee77c7
harish <harishkumaaran23@gmail.com>
2021-10-01 13:59:45 +0530

    Clock V0

---
+++ README.md
@@ -0,0 +1,17 @@
+# Unifeed Blog Post Challenge
+
+This is a Challenge accepted from [Wesbos30](https://www.youtube.com/watch?v=xu87YWbr4X0&list=PLu8EoSxDXHP6CGK4YVJhL_VWetA865GOH&index=3). It is static Landing page designed using Tailwind css.
+
+[**click here to view the component**]()
+&nbsp;
+
+
+# Technology Stack
+- HTML
+- CSS
+- JS
+
+&nbsp;
+
+# Credits
+- This Clock is done as a part of my Front end curriculum at Aekam Labs, Coimbatore.
---
+++ app.js
@@ -0,0 +1,26 @@
+var secondHand= document.querySelector('.second-hand');
+var minuteHand= document.querySelector('.minute-hand');
+var hourHand=   document.querySelector('.hour-hand');
+
+function setdate(){
+    var now= new Date();
+    
+    var seconds= now.getSeconds();
+    var secondDegree= ((360/60)*seconds)+90;
+    secondHand.style.transform = `rotate(${secondDegree}deg)`;
+    
+    var mins = now.getMinutes();
+    var minsDegree = ((360/60)*mins)+90;
+    minuteHand.style.transform = `rotate(${minsDegree}deg)`;
+
+    var hours = now.getHours();
+    if(hours>12)
+    {
+        hours=hours-12;
+    }
+    var hourDegree = ((360/60)*hours)+90;
+    hourHand.style.transform = `rotate(${hourDegree}deg)`;
+
+}
+
+setInterval(setdate,1000);
